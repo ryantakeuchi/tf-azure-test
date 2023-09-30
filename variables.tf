0022738ad2473a6c649f8674f7f34df941df7528
ryantakeuchi <ryantakeuchi@live.com>
2023-09-30 15:18:03 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -7,3 +7,13 @@ variable tenant_id {
   type = string
   description = "Azure Tenant ID"
 }
+
+variable client_id {
+  type = string
+  description = "Azure Service Principal appId"
+}
+
+variable client_secret {
+  type = string
+  description = "Azure Service Principal password"
+}
