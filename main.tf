e67336ab0c351ab4653f6ee2db5b5aa1e741f41d
ryantakeuchi <ryantakeuchi@live.com>
2023-09-30 15:15:31 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,10 +8,13 @@ terraform {
 }
 
 # 2. Configure the AzureRM Provider
-provider "azurerm" {
-  features {}
-  subscription_id = var.subscription_id
-  tenant_id = var.tenant_id
+provider "azurerm" { 
+   subscription_id = var.subscription_id 
+   tenant_id       = var.tenant_id 
+   client_id       = var.client_id 
+   client_secret   = var.client_secret 
+   # Provider Configuration options 
+   features {} 
 }
 
 # 4. Create a virtual network within the resource group
