3d02ef77f624ce1d8bed7252b70433e1c7e5f2ad
ryantakeuchi <ryantakeuchi@live.com>
2023-09-30 20:52:33 -0700

    typo

--- main.tf
+++ main.tf
@@ -33,7 +33,7 @@ provider "azurerm" {
 # 4. Create a virtual network within the resource group
 resource "azurerm_virtual_network" "example" {
   name                = "example-network"
-  resource_group_name = "azure-demo-rt-test-${random_id.server.hex}"
+  resource_group_name = "azure-demo-rt-test-${random_id.id.hex}"
   location            = "West US 2"
   address_space       = ["172.16.0.0/16"]
 }
